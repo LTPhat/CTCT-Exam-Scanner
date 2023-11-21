9c237f54f6935560c7a2bf9979a7e2e512fa1829
LTPhat <lamthanhphat0918398911@gmail.com>
2023-11-21 17:45:18 +0700

    extract mssv completed

--- header_processing.py
+++ header_processing.py
@@ -4,14 +4,17 @@ from offset import Params
 import math
 from collections import defaultdict
 from model.mnist_model import DigitModel
-
+from model.model import CNN_Model
 
 offset = Params()
 
 
 
 
-def get_MSSV(model, header_img):
+def get_MSSV_by_model(model, header_img):
+    """
+    Method 1: Using handwriting recognition (doesn't work well)
+    """
     if (np.array(header_img).shape[2] != 1):
         gray_img = cv2.cvtColor(header_img, cv2.COLOR_BGR2GRAY)
 
@@ -39,9 +42,6 @@ def get_MSSV(model, header_img):
         digit_img = mssv_line_img[:,i * offset: (i+1) * offset]
         digit_img = cv2.threshold(digit_img, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
         digit_img = cv2.resize(digit_img, (16, 16))
-        print(digit_img.shape)
-        cv2.imshow("1", digit_img)
-        cv2.waitKey(0)
         digit_list.append(digit_img)
 
     # Get prediction
@@ -56,16 +56,62 @@ def get_MSSV(model, header_img):
 
 
 
-if __name__ == "__main__":
-    model = DigitModel(weight_path= "./model/mnist_weight.h5").build_model(rt=True)
+def get_mssv_by_processing(model, header_img):
+    """
+    Method 2: Image processing (work well)
+    """
+    if (np.array(header_img).shape[2] != 1):
+        gray_img = cv2.cvtColor(header_img, cv2.COLOR_BGR2GRAY)
 
-    img_dir = "GiayThi-4.png"
+    #Gassian blur
+    blured = cv2.GaussianBlur(gray_img, (5,5), 0)
+    edged = cv2.Canny(blured, 150, 200)
+    
+    contours, hierarchy = cv2.findContours(edged,  cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours) > 0:
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    # Get MSSV block
+    mssv = contours[1]
+    x, y, w, h = cv2.boundingRect(mssv)
+    mssv_img = gray_img[y: y+h, x: x+ w]
+    
+    # Height and width of each bubble
+    mssv_line_height = math.ceil(mssv_img.shape[0] // 11)
+    mssv_line_width = math.ceil(mssv_img.shape[1] // 7)
+
+    digit_list = []
+    for i in range(7):
+        # Get a column
+        col_img = mssv_img[:, i * mssv_line_width : (i + 1) * mssv_line_width]
+        # Get bubles each column
+        for j in range(11):
+            # Ignore handwriting bubble
+            if j == 0:
+                continue
+            digit_img = col_img[j * mssv_line_height: (j+1) * mssv_line_height, :]
+            digit_img = cv2.threshold(digit_img, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+            digit_img = cv2.resize(digit_img, (20, 20))
+            digit_list.append(digit_img)
+    
+    # Get prediction
+    digit_list = np.array(digit_list)
+    scores = model.predict(digit_list / 255.0)
+    mssv = ""
+    for i, score in enumerate(scores):
+        idx = np.argmax(score)
+        if idx == 1:
+            mssv += str(i % 10)
+    
+    return mssv
+    
+
+
+if __name__ == "__main__":
+    model = CNN_Model(weight_path= "./model/weight.h5").build_model(rt=True)
+    img_dir = "./samples/8.jpg"
     res = cv2.imread(img_dir)
     res = cv2.resize(res, offset.resize_shape)
-
     header = res[:offset.header_offset, :]
-    # cv2.imshow("header", header)
-    # cv2.waitKey(0)
-
-    mssv = get_MSSV(model, header)
+    mssv = get_mssv_by_processing(model, header)
     print(mssv)
